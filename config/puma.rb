f72b37e00a611ee23a092cba6fd391ccf4078627
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-09 10:58:16 -0600

    corrected the puma.rb file

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,3 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
@@ -12,4 +11,5 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/
   # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  Activ
+  ActiveRecord::Base.establish_connection
+end
