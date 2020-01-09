3e9a80c64fd8b59c05170b5b8351af48d82f624c
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-09 04:59:37 -0600

    worked on the gravatar image and the sidebar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
+    # debugger
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
+    # puts params.to_yaml
   end
 end
