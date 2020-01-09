536a894dbcd434ef8b5ca2c0bb98121608ab1cb8
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-09 05:13:05 -0600

    created the signup page form using the helper form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
   def new
-    # debugger
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
-    # debugger
-    # puts params.to_yaml
   end
 end
