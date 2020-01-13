9eeb44466d86f997fcf974b0a94b9cc206733656
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-13 05:24:31 -0600

    added a user login test case

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -40,7 +40,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_equal FILL_IN, assigns(:user).FILL_IN
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
   end
 
   test "login without remembering" do
