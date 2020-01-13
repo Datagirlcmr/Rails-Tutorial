baf68389f7cf65695d571e8f8dc3aa25f7b10d0c
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-13 07:49:18 -0600

    created the edit userview, tested for unsuccessful edits

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @user = users(:selma)
   end
   
   test "login with invalid information" do
