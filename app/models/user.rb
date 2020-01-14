4729506cadc2e06ddfc63673d05267b19cb1ccd0
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-14 16:07:23 -0600

    added a default url for mail delivery in test.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User < ApplicationRecord
 
   # Activates an account.
   def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # Sends activation email.
