d40d035925150a7b5ae95b8b6488649aa46d1638
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-20 08:43:51 -0600

    created the micropostmodel and ran the associated tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless (@user.activated)
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
