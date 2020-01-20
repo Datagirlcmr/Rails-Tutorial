7ca0e265aa73882d50d83e133f30574f8e9e4109
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-20 15:21:28 -0600

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,15 +54,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)   
     end
 
-     # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
      # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
